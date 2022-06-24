325854ed17db21b3ef59c70a91d659a9423b78f2
jnu3d1 <jnu3d1@rambler.ru>
2022-06-24 18:47:46 +0600

    Зарегистрирована модель Task

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from webapp.models import Task
+
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status']
+    list_filter = ['status']
+    search_fields = ['description']
+    fields = ['description', 'status', 'completion_date', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+admin.site.register(Task, TasksAdmin)
