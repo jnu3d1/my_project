e960b0e363e3c24320ac07d76388cfbb36680679
jnu3d1 <jnu3d1@rambler.ru>
2022-06-24 18:23:06 +0600

    Отредактированы файлы webapp/models.py и webapp/views.py Создана миграция

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    description = models.TextField(max_length=3000, verbose_name='Описание')
+    status = models.CharField(max_length=50, verbose_name='Статус', default='new')
+    completion_date = models.CharField(max_length=50, blank=True, verbose_name='Дата выполнения')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    def __str__(self):
+        return f'{self.id}. {self.description}: {self.status}'
+
+    class Meta:
+        db_table = 'tasks'
+        verbose_name = "Задача"
+        verbose_name_plural = 'Задачи'
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from webapp.models import Task
+
+def index(request):
+    pass
+
+def create_task(request):
+    pass
+
+def task_view(request):
+    pass
