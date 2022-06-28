8657f81ca3633e8a5fff91b02ec06eb4abd5a04b
jnu3d1 <jnu3d1@rambler.ru>
2022-06-28 17:06:36 +0600

    Занятие 46 Изменения, которые включены в коммит: 	изменено:      webapp/templates/index.html 	изменено:      webapp/urls.py 	изменено:      webapp/views.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from webapp.views import index, create_task, task_view, delete_task
 
 urlpatterns = [
-    path('', index),
-    path('tasks/add/', create_task),
-    path('task/', task_view),
-    path('tasks/delete/', delete_task),
+    path('', index, name='index'),
+    path('tasks/add/', create_task, name='create_task'),
+    path('task/<int:pk>/', task_view, name='task_view'),
+    # path('task/', task_view),
+    path('tasks/delete/', delete_task, name='delete_task'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponseNotFound
+from django.urls import reverse
 
 # Create your views here.
 
@@ -17,12 +19,20 @@ def create_task(request):
         status = request.POST.get('status')
         completion_date = request.POST.get('completion_date')
         new_task = Task.objects.create(description=description, status=status, completion_date=completion_date)
-        context = {'task': new_task}
-        return render(request, 'task_view.html', context)
+        # context = {'task': new_task}
+        return redirect('task_view', pk=new_task.pk)
+        # return HttpResponseRedirect(reverse('task_view', kwargs={'pk': new_task.pk}))
+        # return HttpResponseRedirect(f'/task/{new_task.pk}')
+        # return HttpResponseRedirect(f'/task?pk={new_task.pk}')
+        # return render(request, 'task_view.html', context)
 
-def task_view(request):
-    pk = request.GET.get('pk')
-    task = Task.objects.get(pk=pk)
+def task_view(request, pk):
+    # pk = request.GET.get('pk')
+    # task = get_object_or_404(Task, pk=pk)
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return HttpResponseNotFound('Страница не найдена')
     return render(request, 'task_view.html', {'task': task})
 
 def delete_task(request):
