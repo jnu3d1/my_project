bc2fe47d9f9362aa8a178a08a7743dfb1ce5b720
jnu3d1 <jnu3d1@rambler.ru>
2022-07-06 17:28:44 +0600

    Создана новая модель Изменения, которые включены в коммит: 	новый файл:    webapp/migrations/0006_comment.py 	изменено:      webapp/models.py

--- webapp/models.py
+++ webapp/models.py
@@ -3,7 +3,15 @@ from django.db import models
 
 # Create your models here.
 
-class Task(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+
+    class Meta:
+        abstract = True
+
+
+class Task(BaseModel):
     status_choices = [
         ('new', 'Новая'),
         ('in_progress', 'В процессе'),
@@ -12,8 +20,6 @@ class Task(models.Model):
     description = models.TextField(max_length=3000, verbose_name='Описание')
     status = models.CharField(max_length=11, verbose_name='Статус', choices=status_choices, default='new')
     completion_date = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
 
     def __str__(self):
         return f'{self.id}. {self.title}: {self.status}'
@@ -22,3 +28,18 @@ class Task(models.Model):
         db_table = 'tasks'
         verbose_name = 'Задача'
         verbose_name_plural = 'Задачи'
+
+
+class Comment(BaseModel):
+    text = models.TextField(max_length=400, verbose_name='Комментарий')
+    author = models.CharField(max_length=40, null=True, blank=True, default='Аноним', verbose_name='Автор')
+    task = models.ForeignKey('webapp.Task', on_delete=models.CASCADE, related_name='comments', verbose_name='Статья')
+
+    def __str__(self):
+        return f'{self.id}. {self.author}: {self.text}'
+
+    class Meta:
+        db_table = 'comments'
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
